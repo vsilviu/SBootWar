2462ac43b34ce9db0b7188085dd84e40a25be818
Silviu <v@f.c>
2016-08-31 23:19:12 +0300

    added starter class

---
+++ deployAction.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+/Library/Tomcat/bin/shutdown.sh &&
+rm -rf /usr/local/apache-tomcat-9.0.0.M9/logs/* &&
+rm -rf /usr/local/apache-tomcat-9.0.0.M9/webapps/testwar &&
+rm -rf /usr/local/apache-tomcat-9.0.0.M9/webapps/testwar.war &&
+cp /Users/Silviu/IdeaProjects/Spring\ Boot\ War/Module_One/target/testwar.war /usr/local/apache-tomcat-9.0.0.M9/webapps &&
+/Library/Tomcat/bin/startup.sh
